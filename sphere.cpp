79d8460f03ccbc1e7eeef233eb48045f7a95b56b
Rune Sørland Monstad <rmo019@post.uit.no>
2016-08-11 17:02:42 +0200

    README, trenger bedre forklaringer om klassen

--- sphere.cpp
+++ sphere.cpp
@@ -2,32 +2,9 @@
 #include<eigen3/Eigen/Core>
 #include<vector>
 #include<fstream>
-//#include<plot.hpp>
+#include"sphere.h"
 
 
-class Sphere{
-  private:
-    Eigen::Vector3d center;
-    double radius;
-  public:
-    Sphere  ();
-    Sphere  (const Eigen::Vector3d &a , double b) ; // const og & når du har lært det!!!!
-    Sphere (const Sphere & sph ); //copy constructor,,,,les deg opp!!!
-    //Sphere& operator=(const Sphere &sph);//se nermere på, overloading.
-  Eigen::Vector3d get_center() const {
-    return center;
-  }
-  double get_radius () const {
-    return radius;
-  }
-
-  //making the output plottable for gnuplot
-  friend std::ostream & operator<<( std::ostream & output, const Sphere & sph){
-    output  <<  sph.center(0) << " " << sph.center(1) << " " << sph.center(2) << " " <<  sph.radius ;
-    return output;
-  }
-
-};
 
 //default constructor
 Sphere::Sphere(){
